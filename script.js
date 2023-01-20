0b152044107328d2e75756e714d6dd91cb49d3d1
charlerae <contactcharlerae@gmail.com>
2023-01-19 21:49:40 -0500

    adds functionality to dark mode/light mode functionality

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 
+// Select Toogle Switch
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+
+// Switch Theme
+function switchTheme(event) {
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+    console.log(event.target.checked);
+}
+
+// Event Listener
+toggleSwitch.addEventListener('change', switchTheme);
 
 // https://www.charleraecafe.com/
