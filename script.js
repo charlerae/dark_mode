b751413c8e22bd6e43de7357bde507f24bf32256
charlerae <contactcharlerae@gmail.com>
2023-01-22 22:36:42 -0500

    updates functionality of light/dark toggle

--- script.js
+++ script.js
@@ -8,12 +8,30 @@ const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
+// Dark Mode 
+function darkMode() {
+    nav.style.backgroundColor = 'rgb(0 0 0/50%)';
+    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%';
+    toggleIcon.children[0].textContent = 'Dark Mode';
+    toggleIcon.children[1].classList.remove('fa-sun');
+    toggleIcon.children[1].classList.add('fa-moon');
+}
+
+// Light Mode
+function lightMode() {
+    toggleIcon.children[0].textContent = 'Light Mode';
+    toggleIcon.children[1].classList.remove('fa-moon');
+    toggleIcon.children[1].classList.add('fa-sun');
+}
+
 // Switch Theme
 function switchTheme(event) {
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        darkMode();
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        lightMode();
     }
     console.log(event.target.checked);
 }
