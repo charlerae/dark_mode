e912599f498cb1ff2e8d398a022f75a3776e5245
charlerae <contactcharlerae@gmail.com>
2023-01-23 17:43:07 -0500

    refactors darkMode and lightMode functions

--- script.js
+++ script.js
@@ -8,16 +8,20 @@ const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
+// Changes Images dark/light
+function imageMode(mode) {
+    image1.src = `img/developer_${mode}.svg`;
+    image2.src = `img/engineering_${mode}.svg`;
+    image3.src = `img/cloud_${mode}.svg`;
+}
+
 // Dark Mode 
 function darkMode() {
     nav.style.backgroundColor = 'rgb(0 0 0/50%)';
     textBox.style.backgroundColor = 'rgb(255 255 255 / 50%';
     toggleIcon.children[0].textContent = 'Dark Mode';
-    toggleIcon.children[1].classList.remove('fa-sun');
-    toggleIcon.children[1].classList.add('fa-moon');
-    image1.src = 'img/developer_dark.svg';
-    image2.src = 'img/engineering_dark.svg';
-    image3.src = 'img/cloud_dark.svg';
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+    imageMode('dark');
 }
 
 // Light Mode
@@ -25,11 +29,8 @@ function lightMode() {
     nav.style.backgroundColor = 'rgb(255 255 255 / 50%';
     textBox.style.backgroundColor = 'rgb(0 0 0/50%)';
     toggleIcon.children[0].textContent = 'Light Mode';
-    toggleIcon.children[1].classList.remove('fa-moon');
-    toggleIcon.children[1].classList.add('fa-sun');
-    image1.src = 'img/developer_light.svg';
-    image2.src = 'img/engineering_light.svg';
-    image3.src = 'img/cloud_light.svg';
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    imageMode('light');
 }
 
 // Switch Theme
