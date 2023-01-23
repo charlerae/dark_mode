57754ba248d53ddcfc4d1f40f08c6acbce635793
charlerae <contactcharlerae@gmail.com>
2023-01-22 22:41:30 -0500

    updates functionality of toggle for textbox under buttons

--- script.js
+++ script.js
@@ -15,13 +15,21 @@ function darkMode() {
     toggleIcon.children[0].textContent = 'Dark Mode';
     toggleIcon.children[1].classList.remove('fa-sun');
     toggleIcon.children[1].classList.add('fa-moon');
+    image1.src = 'img/developer_dark.svg';
+    image2.src = 'img/engineering_dark.svg';
+    image3.src = 'img/cloud_dark.svg';
 }
 
 // Light Mode
 function lightMode() {
+    nav.style.backgroundColor = 'rgb(255 255 255 / 50%';
+    textBox.style.backgroundColor = 'rgb(0 0 0/50%)';
     toggleIcon.children[0].textContent = 'Light Mode';
     toggleIcon.children[1].classList.remove('fa-moon');
     toggleIcon.children[1].classList.add('fa-sun');
+    image1.src = 'img/developer_light.svg';
+    image2.src = 'img/engineering_light.svg';
+    image3.src = 'img/cloud_light.svg';
 }
 
 // Switch Theme
